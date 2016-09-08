bb540dd1ff3b578ed572e87b065cf49b9547a25d
Juan Garcia <garc2155@M506-246-25987.local>
2016-09-08 15:39:24 -0700

    Followed videos until the culmination of the vid2, and added some notes for the homework including pi, sin and cos. Note: need research for adding . for floating point numbers

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -32,16 +32,42 @@ class ViewController: UIViewController {
         }
         
         switch operation {
-            case "×":
-                if operandStack.count >= 2 {
-                    displayValue = operandStack.removeLast() * operandStack.removeLast()
-                    enter()
-                }
+        case "×": performOperation { $0 * $1 }
+        case "÷": performOperation { $1 / $0 }
+        case "+": performOperation { $0 + $1 }
+        case "−": performOperation { $1 - $0 }
+        case "√": performOperation { sqrt($0) }
+            // if pi where to appear here how would it work?
+            // one thought is that it would be multiplied by whatever was the last value entered
+            // another is that it would get worked out as a normal number and added to the stack
+            // in either case consider the following:
+            //  1: if the stack is empty and the user presses pi the value of pi will be entered
+            //      in the stack and pi will be displayed aka 3.14159...
+            //  2: if the user enters a number like 123 and then presses the pi button will the
+            //      current value be multiplied by pi and then added to the stack or will the 
+            //      number be added, then pi and then the multiplication button is pressed
+            //      (that last bit sounded like a good idea)
+            //  3: swift can now call pi by using the following Double.pi, this also applies to 
+            //      Float and CGFloat
             
-//            case "÷":
-//            case "+":
-//            case "−":
+            // for sine and cosine this was a suggestion online:
+            // import Foundation
+            // var theCosOfZero: Double = Double(cos(0) // should equal 1
             default: break
+        }
+    }
+    
+    func performOperation (operation: (Double, Double) -> Double) {
+        if operandStack.count >= 2 {
+            displayValue = operation(operandStack.removeLast(), operandStack.removeLast())
+            enter()
+        }
+    }
+    
+    func performOperation (operation: Double -> Double) {
+        if operandStack.count >= 1 {
+            displayValue = operation(operandStack.removeLast())
+            enter()
         }
     }
     

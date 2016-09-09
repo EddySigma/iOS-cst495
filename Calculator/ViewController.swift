6cbdbf8f20a717eef798b072f59b2ff177b0e231
Juan Eduardo Garcia <darkPhoenix@mac-pro.attlocal.net>
2016-09-09 08:32:43 -0700

    Completed functionality for ".", pi, sine and cosine

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -6,6 +6,7 @@
 //  Copyright © 2016 Juan Garcia. All rights reserved.
 //
 
+import Foundation
 import UIKit
 
 class ViewController: UIViewController {
@@ -13,6 +14,7 @@ class ViewController: UIViewController {
     @IBOutlet weak var display: UILabel!
     
     var userIsInTheMiddleOfTypingNumber = false
+    var usingRadixPoint = false
     
     @IBAction func appendDigit(sender: UIButton) {
         // declare a local var
@@ -25,6 +27,20 @@ class ViewController: UIViewController {
         }
     }
     
+    @IBAction func radixPoint() {
+        if userIsInTheMiddleOfTypingNumber {
+            if !usingRadixPoint {
+                display.text = display.text! + "."
+            }
+        } else {
+            if !usingRadixPoint{
+                display.text = "0."
+            }
+            userIsInTheMiddleOfTypingNumber = true
+        }
+        usingRadixPoint = true
+    }
+        
     @IBAction func operate(sender: UIButton) {
         let operation = sender.currentTitle!
         if userIsInTheMiddleOfTypingNumber {
@@ -37,23 +53,21 @@ class ViewController: UIViewController {
         case "+": performOperation { $0 + $1 }
         case "−": performOperation { $1 - $0 }
         case "√": performOperation { sqrt($0) }
-            // if pi where to appear here how would it work?
-            // one thought is that it would be multiplied by whatever was the last value entered
-            // another is that it would get worked out as a normal number and added to the stack
-            // in either case consider the following:
-            //  1: if the stack is empty and the user presses pi the value of pi will be entered
-            //      in the stack and pi will be displayed aka 3.14159...
-            //  2: if the user enters a number like 123 and then presses the pi button will the
-            //      current value be multiplied by pi and then added to the stack or will the 
-            //      number be added, then pi and then the multiplication button is pressed
-            //      (that last bit sounded like a good idea)
-            //  3: swift can now call pi by using the following Double.pi, this also applies to 
-            //      Float and CGFloat
-            
-            // for sine and cosine this was a suggestion online:
-            // import Foundation
-            // var theCosOfZero: Double = Double(cos(0) // should equal 1
-            default: break
+        case "Sin": performOperation { sin($0) }
+        case "Cos": performOperation { cos($0) }
+        default: break
+        }
+    }
+    
+    @IBAction func pi() {
+        if display.text == "0" {
+            display.text = "\(M_PI)"
+            enter()
+        }
+        else {
+            enter()
+            operandStack.append(M_PI)
+            performOperation { $0 * $1 }
         }
     }
     
@@ -64,7 +78,7 @@ class ViewController: UIViewController {
         }
     }
     
-    func performOperation (operation: Double -> Double) {
+    private func performOperation (operation: Double -> Double) {
         if operandStack.count >= 1 {
             displayValue = operation(operandStack.removeLast())
             enter()
@@ -76,6 +90,9 @@ class ViewController: UIViewController {
     @IBAction func enter() {
         userIsInTheMiddleOfTypingNumber = false
         operandStack.append(displayValue)
+        if usingRadixPoint {
+            usingRadixPoint = false
+        }
         print("operandStack = \(operandStack)")
     }
     

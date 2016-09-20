9a0d6007dd1c4ab78596b3086c7a2dd2cc7337f6
Juan Eduardo Garcia <darkPhoenix@Mac-Pro.attlocal.net>
2016-09-19 22:53:35 -0700

    followed video 3 completelly. However a problem has appeared where in the file AppDelegate.swift the function application has an error stating 'use of undeclared type UIApplicationLaunchOptionsKey'

--- CalculatorFiles/Calculator/CalculatorBrain.swift
+++ CalculatorFiles/Calculator/CalculatorBrain.swift
@@ -13,8 +13,8 @@ class CalculatorBrain
 {
     private enum Op {
         case Operand(Double)
-        case UnaryOperation(String, (Double) -> Double)
-        case BinaryOperation(String, (Double, Double) -> Double)
+        case UnaryOperation(String, (Double) -> Double) // string containing symbol and function that returns a double
+        case BinaryOperation(String, (Double, Double) -> Double) // same as above but the function takes two items and still returns a double
     }
     // api aplication programing interface
     
@@ -31,28 +31,56 @@ class CalculatorBrain
         knownOps["√"] = Op.UnaryOperation("√", sqrt)
     }
     
-    func evaluate(ops: [Op]) -> (result: Double?, remaining: [Op])
+    // the evaluate functions are used recursively where depending on the value of the
+    // item in the stack they may look at one item or the entire stack to complete the 
+    // the calculations or return an error or a message if it is not possible to return
+    // something
+    private func evaluate(ops: [Op]) -> (result: Double?, remainingOps: [Op])
     {
-        var ops = ops
-        if !0ps.isEmpty {
-            let op = ops.removeLast()
+        if !ops.isEmpty {
+            var remainingOps = ops
+            let op = remainingOps.removeLast()
+            
+            switch op {
+            case .Operand(let operand):
+                return (operand, remainingOps)
+            case .UnaryOperation(_, let operation):
+                let operandEvaluation = evaluate(remainingOps)
+                if let operand =  operandEvaluation.result {
+                    return (operation(operand), operandEvaluation.remainingOps)
+                }
+            case .BinaryOperation(_, let operation):
+                let op1Evaluation = evaluate(remainingOps)
+                if let operand1 = op1Evaluation.result {
+                    let op2Evaluation = evaluate(op1Evaluation.remainingOps)
+                    if let operand2 = op2Evaluation.result {
+                        return (operation(operand1, operand2), op2Evaluation.remainingOps)
+                    }
+                }
+            }
+            // there is no break because all cases were taken care of
         }
         
         return (nil, ops)
     }
     
     func evaluate() -> Double? {
-        
+        // let (result, remainder) = evaluate(opStack) => here remainder is replaced because we do not care for remainder
+        // as it is never used for this function
+        let (result, _) = evaluate(opStack)
+        return result
     }
     
     
-    func pushOperand(operand: Double) {
+    func pushOperand(operand: Double) -> Double? {
         opStack.append(Op.Operand(operand))
+        return evaluate() // is this the best thing to do... for now yes.
     }
     
-    func performOperation(symbol: String){
+    func performOperation(symbol: String) -> Double? {
         if let operation = knownOps[symbol] {
             opStack.append(operation)
         }
+        return evaluate()
     }
 }
--- CalculatorFiles/Calculator/ViewController.swift
+++ CalculatorFiles/Calculator/ViewController.swift
@@ -14,9 +14,10 @@ class ViewController: UIViewController {
     @IBOutlet weak var display: UILabel!
     
     var userIsInTheMiddleOfTypingNumber = false
+    var brain = CalculatorBrain()
     var usingRadixPoint = false
     
-    @IBAction func appendDigit(_ sender: UIButton) {
+    @IBAction func appendDigit(sender: UIButton) {
         // declare a local var
         let digit = sender.currentTitle!
         if userIsInTheMiddleOfTypingNumber {
@@ -41,24 +42,22 @@ class ViewController: UIViewController {
         usingRadixPoint = true
     }
         
-    @IBAction func operate(_ sender: UIButton) {
-        let operation = sender.currentTitle!
+    @IBAction func operate(sender: UIButton) {
         if userIsInTheMiddleOfTypingNumber {
             enter()
         }
-        
-        switch operation {
-        case "×": performOperation { $0 * $1 }
-        case "÷": performOperation { $1 / $0 }
-        case "+": performOperation { $0 + $1 }
-        case "−": performOperation { $1 - $0 }
-        case "√": performOperation { sqrt($0) }
-        case "Sin": performOperation { sin($0) }
-        case "Cos": performOperation { cos($0) }
-        default: break
+        if let operation = sender.currentTitle {
+            if let result = brain.performOperation(operation) {
+                displayValue = result
+            } else {
+                displayValue = 0 // you want to give the capacity of taking nils. For hw
+            }
         }
+        
     }
     
+    // pi and radix most be updated to acomodate the new calculatorBrain
+    /*
     @IBAction func pi() {
         if display.text == "0" {
             display.text = "\(M_PI)"
@@ -69,38 +68,27 @@ class ViewController: UIViewController {
             operandStack.append(M_PI)
             performOperation { $0 * $1 }
         }
-    }
+    }*/
     
-    func performOperation (_ operation: (Double, Double) -> Double) {
-        if operandStack.count >= 2 {
-            displayValue = operation(operandStack.removeLast(), operandStack.removeLast())
-            enter()
-        }
-    }
-    
-    fileprivate func performOperation (_ operation: (Double) -> Double) {
-        if operandStack.count >= 1 {
-            displayValue = operation(operandStack.removeLast())
-            enter()
-        }
-    }
-    
-    // internal stack that contains the numbers
-    var operandStack = Array<Double>()
     @IBAction func enter() {
         userIsInTheMiddleOfTypingNumber = false
-        operandStack.append(displayValue)
+        if let result = brain.pushOperand(displayValue) {
+            displayValue = result
+        } else {
+            displayValue = 0
+        }
+        
+        /* previous message applies here too
         if usingRadixPoint {
             usingRadixPoint = false
-        }
-        print("operandStack = \(operandStack)")
+        }*/
     }
     
     // computed property example
     var displayValue: Double {
         get {
             // figure out what goes here
-            return NumberFormatter().number(from: display.text!)!.doubleValue
+            return NSNumberFormatter().numberFromString(display.text!)!.doubleValue
         }
         set {
             display.text = "\(newValue)"
